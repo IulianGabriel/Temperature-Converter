dba30dcae3bc1e10a67c4114765d097c3a2086c7
Burciu Iulian Gabriel <93830469+IulianGabriel@users.noreply.github.com>
2023-04-01 12:00:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -9,12 +9,21 @@ const farenheitTemp = document.querySelector('.input-farenheit');
 const kelvinTemp = document.querySelector('.input-kelvin');
 const rankineTemp = document.querySelector('.input-rankine');
 
+function removeBorder(){
+    celsiusTemp.style.border = 'none';
+    farenheitTemp.style.border = 'none';
+    kelvinTemp.style.border = 'none';
+    rankineTemp.style.border = 'none';
+}
 
 function convertFC(e){
     e.preventDefault();
     if(!celsiusTemp.value){
+        celsiusTemp.style.border = '2px solid red';
         return;
-    }
+        } else {
+            removeBorder();
+        }
     farenheitTemp.value = (celsiusTemp.value * 9 / 5 + 32).toFixed(2);
     kelvinTemp.value = (parseFloat(celsiusTemp.value) + 273.15).toFixed(2);
     rankineTemp.value = ((celsiusTemp.value * 9 / 5) + 491.67).toFixed(2);
@@ -23,7 +32,9 @@ function convertFC(e){
 function convertFF(e){
     e.preventDefault();
     if(!farenheitTemp.value){
-        return;
+        return farenheitTemp.style.border = '2px solid red';
+    } else {
+        removeBorder();
     }
     celsiusTemp.value = ((farenheitTemp.value - 32) * 5 / 9).toFixed(2);
     kelvinTemp.value = (parseFloat(celsiusTemp.value) + 273.15).toFixed(2)
@@ -33,7 +44,9 @@ function convertFF(e){
 function convertFK(e){
     e.preventDefault();
     if(!kelvinTemp.value){
-        return;
+        return kelvinTemp.style.border = '2px solid red';
+    } else {
+        removeBorder();
     }
     celsiusTemp.value = (kelvinTemp.value - 273.15).toFixed(2);
     farenheitTemp.value = ((kelvinTemp.value - 273.15) * 9 / 5 + 32).toFixed(2);
@@ -43,7 +56,9 @@ function convertFK(e){
 function convertFR(e){
     e.preventDefault();
     if(!rankineTemp.value){
-        return;
+        return rankineTemp.style.border = '2px solid red';
+    } else {
+        removeBorder();
     }
     celsiusTemp.value = ((rankineTemp.value - 491.67) * 5 / 9).toFixed(2);
     farenheitTemp.value = (rankineTemp.value - 459.67).toFixed(2);
@@ -56,4 +71,5 @@ function resetIN(e){
     farenheitTemp.value = '';
     kelvinTemp.value = '';
     rankineTemp.value = '';
+    removeBorder();
 }
